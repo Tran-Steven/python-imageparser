12db0f70fb963bd9eab072c59b29846889b00979
Steven Tran <steven29160@gmail.com>
2022-08-20 16:58:16 -0400

    Added directory popup instead of copy and pasting file path Added renaming

--- imageparser.py
+++ imageparser.py
@@ -1,12 +1,27 @@
 import os
+import tkinter
+from tkinter import filedialog
+
+root = tkinter.Tk()
+root.withdraw()
+
+# Popup pick directory with tkinter to file path to edit images ( will not go through subdirectories )
+def search_for_file_path():
+    currdir = os.getcwd()
+    tempdir = filedialog.askdirectory(
+        parent=root, initialdir=currdir, title="Please select a directory:"
+    )
+    if len(tempdir) > 0:
+        print("You chose: %s" % tempdir)
+    return tempdir
 
 
-# Insert full file path to the directory with the images ( will not go through subdirectories )
-file_path = "/Users/steventran/Desktop/imageparser/"
+print("Choose your directory")
+file_path = search_for_file_path() + "/"
+root.destroy()
 
-# loops until all files are gone through
 
-
+# Loop until all specified items are deleted (if yes)
 user_input = input("Do you want to remove your files? (y/yes) (n/no)")
 if user_input in ["y", "Y", "yes", "Yes", "YES"]:
     for file_name in os.listdir(file_path):
@@ -19,9 +34,8 @@ if user_input in ["y", "Y", "yes", "Yes", "YES"]:
         print("Correct file")
     else:
         os.remove(source)
-
 if user_input in ["n", "no", "No", "N"]:
-    print("no files were removed")
+    print("No files were removed")
 
 # Option to rename file, upper,lower, and replace _0
 user_input = input("Do you want to rename your files? (y/yes) (n/no)")
@@ -37,34 +51,29 @@ if user_input in ["y", "Y", "yes", "Yes", "YES"]:
         return formatText[0].under() + "." + formatText[1]
 
     user_input = input(
-        "Do you want it all uppercase or lowercase? (upper) (lower) (none)"
+        "Do you want it all uppercase or lowercase? (u/upper) (l/lower) (n/none)"
     )
-    if user_input == "upper":
+    if user_input in ["u", "upper", "Upper", "U"]:
         for file_name in os.listdir(file_path):
-
             source = file_path + file_name
             os.rename(source, toUpper(file_name))
-            print("upper success")
-    if user_input == "lower":
+            print("Uppercase renaming success")
+    if user_input in ["l", "lower", "Lower", "L"]:
         for file_name in os.listdir(file_path):
-
             source = file_path + file_name
-        os.rename(source, toUnder(file_name))
-        print("lower success")
-    if user_input == "none":
+            os.rename(source, toUnder(file_name))
+            print("Lowercase renaming success")
+    if user_input in ["n", "none", "None", "N"]:
         print("No upper or under were made")
-
     user_input2 = input("remove _0 ? (y,yes) (n/no)")
     if user_input2 in ["y", "Y", "yes", "Yes", "YES"]:
         for file_name in os.listdir(file_path):
-
             source = file_path + file_name
             target = file_name.replace("_0", "")
             os.rename(source, target)
-        print("removed _0")
-    else:
+            print("removed _0 successfully")
+    if user_input2 in ["n", "no", "No", "N"]:
         print("done")
+
 if user_input in ["n", "no", "No", "N"]:
-    print("no files were renamed, program end")
-else:
-    print("unknown command")
+    print("Program ending...")
